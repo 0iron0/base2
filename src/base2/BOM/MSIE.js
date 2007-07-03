ad3e3fc8b5e6e13550f253c089c0249fc2663345
dean.edwards <dean.edwards@4e36c698-df2a-0410-96d9-f3dcf88a0d62>
2007-07-03 21:17:45 +0000

    Fixed lang.bind() again!

--- src/base2/BOM/MSIE.js
+++ src/base2/BOM/MSIE.js
@@ -4,7 +4,7 @@
 if (BOM.detect("MSIE[56].+win") && !BOM.detect("SV1")) {
 	var $closures = {}; // all closures stored here
 	
-	lang.extend("bind", function(method, element) {
+	extend(lang, "bind", function(method, element) {
 		if (!element || element.nodeType != 1) {
 			return this.base(method, element);
 		}
