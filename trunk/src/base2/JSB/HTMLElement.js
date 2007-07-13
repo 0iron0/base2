9461233404dd4e804619175110f54beb75fdca52
dean.edwards <dean.edwards@4e36c698-df2a-0410-96d9-f3dcf88a0d62>
2007-07-13 17:36:53 +0000

    Pass the useCapture argument when adding event listeners.

--- trunk/src/base2/JSB/HTMLElement.js
+++ trunk/src/base2/JSB/HTMLElement.js
@@ -2,7 +2,7 @@
 extend(HTMLElement.prototype, "extend", function(name, value) {
 	// automatically attach event handlers when extending
 	if (!Base._prototyping && Event.PATTERN.test(name) && typeof value == "function") {
-		EventTarget.addEventListener(this, name.slice(2), value);
+		EventTarget.addEventListener(this, name.slice(2), value, false);
 		return this;
 	}
 	if (arguments.length == 2 && name == "style") {
