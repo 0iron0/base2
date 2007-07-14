f4eaff435af1cedbdf11f524268d4a9692007992
dean.edwards <dean.edwards@4e36c698-df2a-0410-96d9-f3dcf88a0d62>
2007-07-14 13:39:09 +0000

    Use new assertArity function (not checked in yet).

--- trunk/src/base2/DOM/strict/EventTarget.js
+++ trunk/src/base2/DOM/strict/EventTarget.js
@@ -1,20 +1,27 @@
 
-extend(EventTarget, {
-	addEventListener: function(target, type, listener) {
-		assert(arguments.length >= 3, "Not enough arguments.", SyntaxError);
-		assert(!target.nodeType, "Invalid target.", TypeError);
-		assertType(listener, "function");
+function validTarget(target) {
+	return target == window || Traversal.isDocument(target) || Traversal.isElement(target);
+};
+
+EventTarget.implement({
+	addEventListener: function(target, type, listener, useCapture) {
+		assertArity(arguments);
+		assert(validTarget(target), "Invalid target.", TypeError);
+		assertType(listener.handleEvent || listener, "function");
+		assertType(useCapture, "boolean");
 		return base(this, arguments);
 	},
 
 	dispatchEvent: function(target, event) {
-		assert(arguments.length >= 2, "Not enough arguments.", SyntaxError);
+		assertArity(arguments);
 		return base(this, arguments);
 	},
 
-	removeEventListener: function(target, type, listener) {
-		assert(arguments.length >= 3, "Not enough arguments.", SyntaxError);
-		assertType(listener, "function");
+	removeEventListener: function(target, type, listener, useCapture) {
+		assertArity(arguments);
+		assert(validTarget(target), "Invalid target.", TypeError);
+		assertType(listener.handleEvent || listener, "function");
+		assertType(useCapture, "boolean");
 		return base(this, arguments);
 	}
 });
