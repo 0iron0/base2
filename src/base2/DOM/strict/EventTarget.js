383eaefe9608b85d1c3a93228bc91c2f565122fd
dean.edwards <dean.edwards@4e36c698-df2a-0410-96d9-f3dcf88a0d62>
2007-07-14 13:52:02 +0000

    Sweet.

--- src/base2/DOM/strict/EventTarget.js
+++ src/base2/DOM/strict/EventTarget.js
@@ -1,27 +1,25 @@
 
-function validTarget(target) {
-	return target == window || Traversal.isDocument(target) || Traversal.isElement(target);
+function assertTarget(target) {
+	assert(target == window || Traversal.isDocument(target) || Traversal.isElement(target), "Invalid event target.", TypeError);
+};
+
+function strictEventListener(target, type, listener, capture) {
+	assertArity(arguments);
+	assertTarget(target);
+	assertType(listener.handleEvent || listener, "function", "Invalid event listener.");
+	assertType(capture, "boolean", "Invalid capture argument.");
+	return base(this, arguments);
 };
 
 EventTarget.implement({
-	addEventListener: function(target, type, listener, useCapture) {
-		assertArity(arguments);
-		assert(validTarget(target), "Invalid target.", TypeError);
-		assertType(listener.handleEvent || listener, "function");
-		assertType(useCapture, "boolean");
-		return base(this, arguments);
-	},
+	addEventListener: strictEventListener,
 
 	dispatchEvent: function(target, event) {
 		assertArity(arguments);
+		assertTarget(target);
+		assert(event && event.type, "Invalid event object.", TypeError);
 		return base(this, arguments);
 	},
 
-	removeEventListener: function(target, type, listener, useCapture) {
-		assertArity(arguments);
-		assert(validTarget(target), "Invalid target.", TypeError);
-		assertType(listener.handleEvent || listener, "function");
-		assertType(useCapture, "boolean");
-		return base(this, arguments);
-	}
+	removeEventListener: strictEventListener
 });
