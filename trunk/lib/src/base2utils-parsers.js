561db2d6583e85023e975ca300f5f75c0c62a923
doekman <doekman@4e36c698-df2a-0410-96d9-f3dcf88a0d62>
2007-07-21 12:48:10 +0000

    Added HTMLEncode and some other stuff

--- trunk/lib/src/base2utils-parsers.js
+++ trunk/lib/src/base2utils-parsers.js
@@ -1,9 +1,9 @@
-// timestamp: Tue, 26 Jun 2007 18:02:32
+// timestamp: Sat, 21 Jul 2007 12:29:40
 
 new function(_) { ////////////////////  BEGIN: CLOSURE  ////////////////////
 
 // =========================================================================
-// base2/namespace.js
+// Parsers/namespace.js
 // =========================================================================
 var Parsers = new base2.Namespace(this, {
 	name:    "Parsers",
@@ -13,23 +13,111 @@ var Parsers = new base2.Namespace(this, {
 
 eval(this.imports);
 // =========================================================================
-// base2/../langcontrib.js
+// Parsers/../langcontrib.js
 // =========================================================================
 function $(id) {
-  return document.getElementById(id);
+  var res=document.getElementById(id);
+  if(res==null) console.log("Element with id '%s' is not found",id);
+  return res;
+}
+function htmlEncode(s) {
+  return String(s).replace(/(&|<|>|")/g,function($,ch) {
+    switch(ch) {
+      case "&": return "&amp;";
+      case "<": return "&lt;";
+      case ">": return "&gt;";
+      case '"': return "&quot;";
+      default : /*should never happen*/ return ch; 
+    }
+  });
 }
 //Don't know how to inherit from Error, so use as:
 //  throw newError("Error number %1.",1)
 function newError(msg/*, format1, ..., format9*/) {
   return new Error(format.apply(null,arguments));
 }
-function bind(object, fn) {
-  return function() {
-    return fn.apply(object,arguments);
+function padRight(str,n,ch) { return (str+new Array(n+1).join(ch||" ")).slice(0,n); }
+function padLeft(str,n,ch) { return (new Array(n+1).join(ch||" ")+str).slice(-n); }
+function toDutchDate(dt,format,bRelative,bCapital,sFirstWord)
+//--#Converteerd een datum-type naar een nederlandse datumstring
+//--@format;type=string@date,datetime,datetimeS,time,timeS
+//--@bRelative;type=boolean;optional;default=false@Use vandaag,gisteren,morgen
+//--@bCapital;type=boolean;optional;default=false@Of het eerste teken een hoofdletter moet zijn.
+//--@sFirstWord;type=string;optional@Het woord waar de datum mee begint (ivm bCapital)
+{
+  var date=fix(dt.getDate())+'-'+fix(dt.getMonth()+1)+'-'+dt.getFullYear();
+  var time=dt.getHours()+':'+fix(dt.getMinutes());
+  var seconds=':'+fix(dt.getSeconds())
+  if(bRelative) {
+    var nDaysFromNow=Date_valueInDays(dt) - Date_valueInDays(new Date());
+    var objRelative={'-1':'gisteren','0':'vandaag','1':'morgen'};
+    if(objRelative[nDaysFromNow]) {
+      date=objRelative[nDaysFromNow];
+      time='om '+time;
+    }
+    else {
+      if(sFirstWord) {
+        date=sFirstWord+' '+date;
+      }
+      time='om '+time;
+    }
+    if(bCapital) {
+      date=date.substr(0,1).toUpperCase()+date.substr(1);
+    }
+  }
+  else {
+    if(time.length==4) time="0"+time;
+  }
+  switch(format) {
+    case 'datetime': return date+' '+time;
+    case 'datetimeS': return date+' '+time+seconds;
+    case 'date': return date;
+    case 'time': return time;
+    case 'timeS': return time+seconds;
+    default: return dt.toString();
   }
 }
+function Date_valueInDays(dt)
+//--#Results in #days since 1970; handy to compares dates without time
+{	
+	return parseInt( new Date(dt.getFullYear(), dt.getMonth(), dt.getDate() ).valueOf() / (24*60*60*1000) );
+}
+
+function fix(n)
+{
+  if((''+n).length==1) return '0'+n;
+  else return ''+n;
+}
+function parseDate(s) {
+  var iso=/^(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2})\.(\d{3})\d+Z$/;
+  var a=iso.exec(s);
+  if(a) return new Date(a[1],a[2],a[3],/*T*/a[4],a[5],a[6],a[7]);
+  return new Date(s);
+}
+/*
+if(console===undefined) {
+  //IE, non-firebug Firefox
+  var console={log:[]}
+  console.log=function(s) { 
+    if(console.log.length==0&&window.attachEvent) {
+      top.defaultStatus+=" [press Ctrl+\\ for logging]";
+    }
+    console.log.push(s); 
+  }
+  if(window.attachEvent) { //IE
+    window.attachEvent("onload",function() {
+      document.body.attachEvent('onkeypress',function() {
+        if(event.keyCode==28) { //control-backslash
+          if(confirm(console.log.join("\n")+"\n\nEmpy log?"))
+            console.log=[];
+        }
+      });
+    });
+  }
+}
+*/
 // =========================================================================
-// base2/uri.js
+// Parsers/uri.js
 // =========================================================================
 //--| according to: http://gbiv.com/protocols/uri/rfc/rfc3986.html
 //
@@ -59,7 +147,7 @@ var csMacro=function(s) {
 		// "OCHAR<xyz>" -> charSets._OCHAR + "xyz" + pct-encoded
 		return "(?:["+charSets["_"+ch+"CHAR"]+args+"]|%[0-9a-zA-Z]{2,2})";
 	});
-}
+};
 //All identifyable parts of an uri we might want to know (items starting with _ have no submatches)
 //TODO: think of semantics vs grammar (the ip-address "400.10.0.1" will get matched as reg_name
 //      instead of IPv4address, since 400 is not a byte/octet0).
@@ -69,7 +157,7 @@ uriParts.SCHEME=         "([a-zA-Z][-+.a-zA-Z0-9]*):";
 uriParts.USERINFO=       csMacro("(?:(OCHAR<:>*)@)?");
 uriParts._dec_octet=     "(?:25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]|[0-9])";
 uriParts._IPv4address=   csMacro("{_dec_octet}\\.{_dec_octet}\\.{_dec_octet}\\.{_dec_octet}");
-uriParts._reg_name=      csMacro("OCHAR<>+")
+uriParts._reg_name=      csMacro("OCHAR<>+");
 uriParts.HOST=           csMacro("({_IPv4address}|{_reg_name})");
 uriParts.PORT=           "(?::(\\d{1,5}))?"; //spec says zero digits, browser says at least one
 uriParts._PATH_ABEMPTY=  csMacro("(?:/PCHAR<>*)*");
@@ -176,8 +264,9 @@ var URIMATCH_NAMES="Scheme,Userinfo,Host,Port,Path1,Path2,Query,Fragment".split(
  sub-delims    = "!" / "$" / "&" / "'" / "(" / ")"
                / "*" / "+" / "," / ";" / "="
 */
+
 // =========================================================================
-// base2/GWInlineParser.js
+// Parsers/GWInlineParser.js
 // =========================================================================
 function ahref(url,innerHtml,cls) { 
   return format('<a href="%1" class="%2">%3</a>',url,cls,innerHtml); 
@@ -220,7 +309,7 @@ var GWInlineParser=RegGrp.extend({
     return result+closed;
   },
   handleCode: function($,code) {
-    return "<code>"+code+"</code>";
+    return "<code>"+htmlEncode(code)+"</code>";
   },
   handleToken: function(token) {
     switch(token) {
@@ -301,7 +390,7 @@ var GWInlineParser=RegGrp.extend({
 });
 
 // =========================================================================
-// base2/State.js
+// Parsers/State.js
 // =========================================================================
 var State=Base.extend({
   constructor: function(name,entryAction,exitAction,context) {
@@ -328,8 +417,7 @@ var State=Base.extend({
         replacement, //replacement
         arguments[2], //nextState
         this, //current state
-        this.context
-      );    
+        this.context);    
     }
     this.stateExits.push(transition);
   },
@@ -338,12 +426,12 @@ var State=Base.extend({
   }
 });
 // =========================================================================
-// base2/StateTransition.js
+// Parsers/StateTransition.js
 // =========================================================================
 var StateTransition=Base.extend({
   constructor: function(pattern, replacement, nextState, currentState, context) {
     this.pattern=instanceOf(pattern,RegExp)?pattern:new RegExp(pattern,"");
-    this.replacement=typeof replacement=="function" ? bind(context, replacement) : replacement;
+    this.replacement=typeof replacement=="function" ? bind(replacement, context) : replacement;
     this.nextState=nextState; //undefined (no transition), null (transition to currentState) or State-object
     this.currentState=currentState;
   },
@@ -354,12 +442,12 @@ var StateTransition=Base.extend({
     return this.nextState!==undefined&&this.nextState!==null;
   },
   performActions: function() { /*this is a question, not a verb*/
-    return this.nextState===null
-        || (this.nextState!==undefined&&this.currentState.name!=this.nextState.name);
+    return this.nextState===null ||
+           (this.nextState!==undefined&&this.currentState.name!=this.nextState.name);
   }
 });
 // =========================================================================
-// base2/FsmParser.js
+// Parsers/FsmParser.js
 // =========================================================================
 //#requires Base2
 //#uses StateTransition, State
@@ -448,16 +536,16 @@ var FsmParser=Base.extend({
   },
   toDotString: function(name) {
     var graph=["/* Visit http://www.graphviz.org/ to create a chart of this text */",format("digraph %1 {",name)];
-    var drawn=new Hash
+    var drawn=new Hash;
     //Visualize start state
-    graph.push(format('\t%1[style=filled,color="#B7D5F6"];',this.startState.name))
+    graph.push(format('\t%1[style=filled,color="#B7D5F6"];',this.startState.name));
     this.states.forEach(function(state, stateName) {
       state.stateExits.forEach(function(transition) {
         //visualize accepting state
         var nextStateName=transition.getNextStateName();
         if(this.states.fetch(nextStateName).isAcceptState&&!drawn.exists(nextStateName)) {
           graph.push(format("\t%1[style=bold]",nextStateName));
-          drawn.store(nextStateName)
+          drawn.store(nextStateName);
         }
         //add transition
         graph.push(format("\t%1 -> %2;", stateName, nextStateName));
@@ -468,8 +556,9 @@ var FsmParser=Base.extend({
   }
   
 });
+
 // =========================================================================
-// base2/GWParser.js
+// Parsers/GWParser.js
 // =========================================================================
 var GWParser=FsmParser.extend({
 	constructor: function() {
@@ -495,7 +584,7 @@ var GWParser=FsmParser.extend({
 		this.addTransitions(G.RX_EMPTY_LINE, "",             [[start],[list,start],[quote,start],[paragraph,start],[table,start]]);
 		this.addTransitions(G.RX_CODE_END,   "</pre>\n",     [[code,start]]);
 		this.addTransitions(G.RX_CODE_END,   "$1",           [[code2,code],[code3,code2]]);
-		this.addTransitions(G.RX_INCODE,     "$1",           [[code],[code2],[code3]]);
+		this.addTransitions(G.RX_INCODE,     this.htmlEncode,[[code],[code2],[code3]]);
     //for graceful closing of incorrect/unclosed markup.
 		this.setAllAsAcceptState();
 		//
@@ -509,6 +598,9 @@ var GWParser=FsmParser.extend({
 		//remove meta-data, normalize linebreaks and add a newline
 		return this.base(s.replace(/^(#.*\n+)*/,'').replace(/\r\n|\r|\n/g,"\n")+"\n");
 	},
+	htmlEncode: function($,match) {
+	  return htmlEncode(match);
+	},
 	//state for lists
 	listLevel: [], //an array of nesting levels, each entry stating the length of the indentation
 	listTypes: [],
@@ -520,7 +612,7 @@ var GWParser=FsmParser.extend({
 	  //TODO: speedup: without the inline parser, the google syntax page takes
 	  //      300ms, and with it 1150ms (including rendering) on my (now stolen)
 	  //      iBook G4 1200MHz 1GB.
-	  return this.inlineParser.exec(text||this.inlineBuffer.join("\n"));
+	  return this.inlineParser.exec(htmlEncode(text||this.inlineBuffer.join("\n")));
 	},
 	pCollect: function($,text) {
 	  this.inlineBuffer.push(text);
@@ -561,7 +653,7 @@ var GWParser=FsmParser.extend({
 		if(this.listLevel.length==0||indent.length>this.listLevel.item(-1)) {
 			this.listTypes.push(bullet=="*"?"ul":"ol");
 			this.listLevel.push(indent.length);
-			res+=format("<%1>\n", this.listTypes.item(-1))
+			res+=format("<%1>\n", this.listTypes.item(-1));
 		}
 		else if(indent.length<this.listLevel.item(-1)) { //collapse
 			res+=this.pCloseLists(indent, 1);
