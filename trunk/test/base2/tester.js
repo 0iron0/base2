72268c8cda665f53b66e81831c458b231564c698
doekman <doekman@4e36c698-df2a-0410-96d9-f3dcf88a0d62>
2007-07-23 15:46:39 +0000

    Created tests for JavaScript/Date2 and created a tester.

---
+++ trunk/test/base2/tester.js
@@ -0,0 +1,59 @@
+var assertEqual = function(a, b, message) {
+  base2.assert(a==b, a+"=="+b+": "+message);
+};
+
+var TestRunner = base2.Base.extend({
+  runTests: function(tests) {
+    var stats=[0,0];
+    for(var name in tests) {
+      var testCase = tests[name];
+      try {
+        testCase();
+        this.log(base2.format("[%1] OK", name), "ok");
+        stats[0]++;
+      } catch(ex) {
+        this.log(base2.format("[%1]: %2", name, ex), "error");
+        stats[1]++;
+      }
+    }
+    this.log(base2.format("Test run completed, %1 OK, %2 error%3", 
+      stats[0], stats[1], stats[1]!=1?"s.":"."), "info");
+  },
+  
+  log: function(text, result) {
+    var li = document.createElement("LI");
+    li.className = result;
+    li.appendChild(document.createTextNode(text));
+    this.getLogger().appendChild(li);
+  },
+  
+  clearLog: function() {
+    this.getLogger().innerHTML='';
+  },
+  
+  getLogger: function() {
+    if (!this.logger) {
+      var logger = document.createElement("UL");
+      logger.id = this.LOGID;
+      this.logger = document.body.appendChild(logger);
+    }
+    return this.logger;
+  },
+  logger: null
+},{
+  LOGID: 'log'  
+});
+
+/*
+var logGroupStart = function(name) {
+  var li = document.createElement("LI");
+  li.appendChild(document.createElement("UL"));
+  li = getLogger().appendChild(li);
+  logger = li.firstChild;
+  log("Group: <strong>"+name+"</strong>", "info");
+};
+
+var logGroupEnd = function() {
+  logger = logger.parentNode.parentNode;
+};
+*/
