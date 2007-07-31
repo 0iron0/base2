284092ca29010c91e99b8c2a313330bec9c5da85
dean.edwards <dean.edwards@4e36c698-df2a-0410-96d9-f3dcf88a0d62>
2007-07-31 00:15:19 +0000

    This doesn't belong here.

--- trunk/src/apps/wf2/Microsoft/src/wf2-admin.php
+++
@@ -1,6 +0,0 @@
-<?php
-include("_classes/admin/Admin.js");
-include("_classes/admin/Console.js");
-include("_classes/admin/Factory.js");
-include("_classes/admin/System.js");
-?>
--- trunk/src/apps/wf2/Microsoft/src/wf2-popups.php
+++
@@ -1,4 +0,0 @@
-<?php
-include("_classes/popups/DataListPopup.js");
-include("_classes/popups/DatePopup.js");
-?>
