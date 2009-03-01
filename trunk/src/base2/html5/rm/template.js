466297e4fdb7b1cdf090659ab26fc54f4bb49d98
dean.edwards <dean.edwards@4e36c698-df2a-0410-96d9-f3dcf88a0d62>
2009-03-01 04:24:16 +0000

    And now it works...

--- trunk/src/base2/html5/rm/template.js
+++ trunk/src/base2/html5/rm/template.js
@@ -2,17 +2,18 @@
 var template = element.extend({
   onattach: function(template) {
     template.style.display = "none";
-    if (template.parentNode && template.parentNode.nodeType == 1) {
-      // maintain the mimimum number of blocks
-      var i = 0;
-      var start = this.get(template, "repeat-start");
-      while (i++ < start) {
-        this.addRepetitionBlock(template, null);
-      };
-      i = this.getRepetitionBlocks(template).length;
-      while (i++ < this.repeatMin) {
-        this.addRepetitionBlock(template, null);
-      }
+    
+    var min   = this.get(template, "repeat-min");
+        start = this.get(template, "repeat-start"),
+        i = 0;
+        
+    while (i++ < start) {
+      this.addRepetitionBlock(template, null);
+    };
+    
+    i = this.getRepetitionBlocks(template).length;
+    while (i++ < min) {
+      this.addRepetitionBlock(template, null);
     }
   },
 
