dfc08c16f1b3ad85b06faf3e82fa02b5dda5618f
dean.edwards <dean.edwards@4e36c698-df2a-0410-96d9-f3dcf88a0d62>
2009-09-23 21:12:49 +0000

    Renamed the "JavaScript" package to "js".
    The JavaScript name is still supported for legacy purposes but is deprecated.

---
+++ trunk/src/base2/js/Date2.js
@@ -0,0 +1,93 @@
+
+// http://developer.mozilla.org/es4/proposals/date_and_time.html
+
+// big, ugly, regular expression
+var _DATE_PATTERN = /^((-\d+|\d{4,})(-(\d{2})(-(\d{2}))?)?)?T((\d{2})(:(\d{2})(:(\d{2})(\.(\d{1,3})(\d)?\d*)?)?)?)?(([+-])(\d{2})(:(\d{2}))?|Z)?$/;
+var _DATE_PARTS = { // indexes to the sub-expressions of the RegExp above
+  FullYear: 2,
+  Month: 4,
+  Date: 6,
+  Hours: 8,
+  Minutes: 10,
+  Seconds: 12,
+  Milliseconds: 14
+};
+var _TIMEZONE_PARTS = { // idem, but without the getter/setter usage on Date object
+  Hectomicroseconds: 15, // :-P
+  UTC: 16,
+  Sign: 17,
+  Hours: 18,
+  Minutes: 20
+};
+
+//var _TRIM_ZEROES   = /(((00)?:0+)?:0+)?\.0+$/;
+//var _TRIM_TIMEZONE = /(T[0-9:.]+)$/;
+
+var Date2 = _createObject2(
+  Date, 
+  function(yy, mm, dd, h, m, s, ms) {
+    switch (arguments.length) {
+      case 0: return new Date;
+      case 1: return typeof yy == "string" ? new Date(Date2.parse(yy)) : new Date(yy.valueOf());
+      default: return new Date(yy, mm, arguments.length == 2 ? 1 : dd, h || 0, m || 0, s || 0, ms || 0);
+    }
+  }, "", {
+    toISOString: function(date) {
+      var string = "####-##-##T##:##:##.###";
+      for (var part in _DATE_PARTS) {
+        string = string.replace(/#+/, function(digits) {
+          var value = date["getUTC" + part]();
+          if (part == "Month") value++; // js month starts at zero
+          return ("000" + value).slice(-digits.length); // pad
+        });
+      }
+      //// remove trailing zeroes, and remove UTC timezone, when time's absent
+      //return string.replace(_TRIM_ZEROES, "").replace(_TRIM_TIMEZONE, "$1Z");
+      return string + "Z";
+    }
+  }
+);
+
+delete Date2.forEach;
+
+Date2.now = function() {
+  return (new Date).valueOf(); // milliseconds since the epoch
+};
+
+Date2.parse = function(string, defaultDate) {
+  if (arguments.length > 1) {
+    assertType(defaultDate, "number", "Default date should be of type 'number'.")
+  }
+  // parse ISO date
+  var parts = match(string, _DATE_PATTERN);
+  if (parts.length) {
+    var month = parts[_DATE_PARTS.Month];
+    if (month) parts[_DATE_PARTS.Month] = String(month - 1); // js months start at zero
+    // round milliseconds on 3 digits
+    if (parts[_TIMEZONE_PARTS.Hectomicroseconds] >= 5) parts[_DATE_PARTS.Milliseconds]++;
+    var utc = parts[_TIMEZONE_PARTS.UTC] || parts[_TIMEZONE_PARTS.Hours] ? "UTC" : "";
+    var date = new Date(defaultDate || 0);
+    if (parts[_DATE_PARTS.Date]) date["set" + utc + "Date"](14);
+    for (var part in _DATE_PARTS) {
+      var value = parts[_DATE_PARTS[part]];
+      if (value) {
+        // set a date part
+        date["set" + utc + part](value);
+        // make sure that this setting does not overflow
+        if (date["get" + utc + part]() != parts[_DATE_PARTS[part]]) {
+          return NaN;
+        }
+      }
+    }
+    // timezone can be set, without time being available
+    // without a timezone, local timezone is respected
+    if (parts[_TIMEZONE_PARTS.Hours]) {
+      var hours = Number(parts[_TIMEZONE_PARTS.Sign] + parts[_TIMEZONE_PARTS.Hours]);
+      var minutes = Number(parts[_TIMEZONE_PARTS.Sign] + (parts[_TIMEZONE_PARTS.Minutes] || 0));
+      date.setUTCMinutes(date.getUTCMinutes() + (hours * 60) + minutes);
+    }
+    return date.valueOf();
+  } else {
+    return Date.parse(string);
+  }
+};
---
+++ trunk/src/base2/js/package.js
@@ -0,0 +1,56 @@
+
+var js = {
+  name:      "js",
+  version:   base2.version,
+  exports:   "Array2,Date2,Function2,String2",
+  namespace: "", // fixed later
+  
+  bind: function(host) {
+    var top = global;
+    global = host;
+    forEach.csv(this.exports, function(name2) {
+      var name = name2.slice(0, -1);
+      extend(host[name], this[name2]);
+      this[name2](host[name].prototype); // cast
+    }, this);
+    global = top;
+    return host;
+  }
+};
+
+function _createObject2(Native, constructor, generics, extensions) {
+  // Clone native objects and extend them.
+
+  // Create a Module that will contain all the new methods.
+  var INative = Module.extend();
+  var id = INative.toString().slice(1, -1);
+  // http://developer.mozilla.org/en/docs/New_in_JavaScript_1.6#Array_and_String_generics
+  forEach.csv(generics, function(name) {
+    INative[name] = unbind(Native.prototype[name]);
+    INative.namespace += format("var %1=%2.%1;", name, id);
+  });
+  forEach (_slice.call(arguments, 3), INative.implement, INative);
+
+  // create a faux constructor that augments the native object
+  var Native2 = function() {
+    return INative(this.constructor == INative ? constructor.apply(null, arguments) : arguments[0]);
+  };
+  Native2.prototype = INative.prototype;
+
+  // Remove methods that are already implemented.
+  for (var name in INative) {
+    var method = Native[name];
+    if (method && name != "prototype" && name != "toString" && method != Function.prototype[name]) {
+      INative[name] = method;
+      delete INative.prototype[name];
+    }
+    Native2[name] = INative[name];
+  }
+  Native2.ancestor = Object;
+  delete Native2.extend;
+  
+  // remove "lang.bind.."
+  Native2.namespace = Native2.namespace.replace(/(var (\w+)=)[^,;]+,([^\)]+)\)/g, "$1$3.$2");
+  
+  return Native2;
+};
