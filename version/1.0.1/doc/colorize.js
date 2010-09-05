d96bfad8039023599338f07f2779e31decb7f3c3
dean.edwards <dean.edwards@4e36c698-df2a-0410-96d9-f3dcf88a0d62>
2010-09-05 18:25:52 +0000

    Version 1.0.1. A bug fix release for base2.DOM.

---
+++ version/1.0.1/doc/colorize.js
@@ -0,0 +1,154 @@
+
+eval(base2.namespace);
+eval(Enumerable.namespace);
+eval(lang.namespace);
+
+var DEFAULT = "@0";
+var IGNORE  = RegGrp.IGNORE;
+
+var Colorizer = RegGrp.extend({
+  constructor: function(patterns, replacements, properties) {
+    this.extend(properties);
+    this.patterns = patterns || {};
+    var values = {}, i;
+    forEach (patterns, function(pattern, className) {
+      values[className] = replacements[className] || DEFAULT;
+    });
+    forEach (replacements, function(replacement, className) {
+      values[className] = replacements[className];
+    });
+    this.base(values);
+  },
+  
+  patterns: null,
+  tabStop: 4,
+  urls: true,
+
+  copy: function() {
+    var colorizer = this.base();
+    colorizer.patterns = copy(this.patterns);
+    return colorizer;
+  },
+  
+  exec: function(text, secondary) {
+    text = this.base(this.escape(text));
+    if (!secondary) { // Not a secondary parse of the text (e.g. CSS within an HTML sample)
+      text = this._parseWhiteSpace(text);
+      if (this.urls) text = Colorizer.urls.exec(text);
+    }
+    return this.unescape(text);
+  },
+
+  escape: function(text) {
+    return String(text).replace(/</g, "\x01").replace(/&/g, "\x02");
+  },
+
+  put: function(pattern, replacement) {
+    // This is a bit complicated and is therefore probably wrong.
+    if (!instanceOf(pattern, RegGrp.Item)) {
+      if (typeof replacement == "string") {
+        replacement = replacement.replace(/@(\d)/, function(match, index) {
+          return format(Colorizer.FORMAT, pattern, index);
+        });
+      }
+      pattern = this.patterns[pattern] || Colorizer.patterns[pattern] || pattern;
+      if (instanceOf(pattern, RegExp)) pattern = pattern.source;
+      pattern = this.escape(pattern);
+    }
+    return this.base(pattern, replacement);
+  },
+
+  unescape: function(text) {
+    return text.replace(/\x01/g, "&lt;").replace(/\x02/g, "&amp;");
+  },
+
+  _parseWhiteSpace: function(text) {
+    // Convert tabs to spaces and then convert spaces to "&nbsp;".
+    var tabStop = this.tabStop;
+    if (tabStop > 0) {
+      var tab = Array(tabStop + 1).join(" ");
+      return text.replace(Colorizer.TABS, function(match) {
+        match = match.replace(Colorizer.TAB, tab);
+        if (tabStop > 1) {
+          var padding = (match.length - 1) % tabStop;
+          if (padding) match = match.slice(0, -padding);
+        }
+        return match.replace(/ /g, "&nbsp;");
+      });
+    }
+    return text;
+  },
+
+  "@MSIE": {
+    _parseWhiteSpace: function(text) {
+      return this.base(text).replace(/\r?\n/g, "<br>");
+    }
+  }
+}, {
+  version: "0.8",
+  
+  FORMAT: '<span class="%1">$%2</span>',
+  DEFAULT: DEFAULT,
+  IGNORE:  IGNORE,  
+  TAB:     /\t/g,
+  TABS:    /\n([\t \xa0]+)/g,
+  
+  init: function() {
+    // Patterns that are defined as Arrays represent
+    // groups of other patterns. Build those groups.
+    forEach (this.patterns, function(pattern, name, patterns) {
+      if (instanceOf(pattern, Array)) {
+        patterns[name] = reduce(pattern, function(group, name) {
+          group.add(patterns[name]);
+          return group;
+        }, new RegGrp);
+      }
+    });
+    this.urls = this.patterns.urls.copy();
+    this.urls.putAt(0, '<a href="mailto:$0">$0</a>');
+    this.urls.putAt(1, '<a href="$0">$0</a>');
+  },
+
+  addScheme: function(name, patterns, replacements, properties) {
+    this[name] = new this(patterns, replacements, properties);
+  },
+  
+  // Pre-defined regular expressions.
+  patterns: {
+    block_comment: /\/\*[^*]*\*+([^\/][^*]*\*+)*\//,
+    email:         /([\w.+-]+@[\w.-]+\.\w+)/,
+    line_comment:  /\/\/[^\r\n]*/,
+    number:        /\b\-?(0|[1-9]\d*)(\.\d+)?([eE][-+]?\d+)?\b/,
+    string1:       /'(\\.|[^'\\])*'/,
+    string2:       /"(\\.|[^"\\])*"/,
+    url:           /(http:\/\/+[\w\/\-%&#=.,?+$]+)/,
+    // groups
+    comment:       ["block_comment", "line_comment"],
+    string:        ["string1", "string2"],
+    urls:          ["email", "url"]
+  },
+  
+  urls: null
+});
+
+base2.addPackage("code");
+base2.code.addName("Colorizer", Colorizer);
+
+with (base2.code.Colorizer) addScheme("javascript", {
+  conditional: /\/\*@if\s*\([^\)]*\)|\/\*@[\s\w]*|@\*\/|\/\/@\w+|@else[\s\w]*/, // conditional comments
+  global:      /\b(clearInterval|clearTimeout|constructor|document|escape|hasOwnProperty|Infinity|isNaN|NaN|parseFloat|parseInt|prototype|setInterval|setTimeout|toString|unescape|valueOf|window)\b/,
+  keyword:     /\b(&&|\|\||arguments|break|case|continue|default|delete|do|else|false|for|function|if|in|instanceof|new|null|return|switch|this|true|typeof|var|void|while|with|undefined)\b/,
+  regexp:      /([\[(\^=,{}:;&|!*?]\s*)(\/(\\\/|[^\/*])(\\.|[^\/\n\\])*\/[mgi]*)/, /* -- */
+  special:     /\b(assert\w*|alert|catch|confirm|console|debug|debugger|eval|finally|prompt|throw|try)\b/
+}, {
+  comment:     DEFAULT,
+  string:      DEFAULT,
+  regexp:      "$1@2",
+  number:      DEFAULT
+});
+
+with (base2.code.Colorizer.javascript) {
+  add("\\b(" + ([base2.exports,base2.JavaScript.exports,lang.exports].join(",") + ",base,base2,merge,union,implement,I,II,K").match(/[^\s,]+/g).join("|") + ")\\b", '<span class="base2">$0</span>');
+  insertAt(0, /("@[^"]+"):/, '<span class="special">$1</span>:');
+  tabStop = 2;
+}
---
+++ version/1.0.1/doc/doc.js
@@ -0,0 +1,121 @@
+/*
+  base2 - copyright 2007-2008, Dean Edwards
+  http://code.google.com/p/base2/
+  http://www.opensource.org/licenses/mit-license.php
+
+  Contributors:
+    Doeke Zanstra
+*/
+
+// timestamp: Sat, 06 Sep 2008 16:52:33
+
+new function(_no_shrink_) { ///////////////  BEGIN: CLOSURE  ///////////////
+
+// =========================================================================
+// doc/package.js
+// =========================================================================
+
+var doc = new base2.Package(this, {
+  name:    "doc",
+  version: "0.5",
+  
+  show: {},
+
+  colorize: function(text) {
+    return Colorizer.javascript.exec(text);
+  }
+});
+
+eval(this.imports);
+
+// =========================================================================
+// doc/data.js
+// =========================================================================
+
+doc.data = new Base({
+  PATH: "/data/doc/entries/",
+  
+  exists: function(objectID, entry) {
+    return MiniWeb.server.io.exists(this.makepath(objectID, entry));
+  },
+
+  makepath: function(objectID, entry) {
+    return this.PATH + String(objectID).replace(/::/, '.prototype.').split('.').join('/') + '/#' + entry;
+  },
+
+  read: function(objectID, entry) {
+    return MiniWeb.server.io.read(this.makepath(objectID, entry));
+  },
+
+  remove: function(objectID, entry) {
+    return MiniWeb.server.io.remove(this.makepath(objectID, entry));
+  },
+  
+  write: function(objectID, entry, value) {
+    var io = MiniWeb.server.io;
+    var names = objectID.replace(/::/, '.prototype.').split('.');
+    for (var i = 1; i <= names.length; i++) {
+      var path = this.PATH + names.slice(0, i).join('/');
+      if (!io.isDirectory(path)) {
+        io.mkdir(path);
+      }
+    }
+    io.write(path + '/#' + entry, value);
+  }
+});
+
+// =========================================================================
+// doc/init.js
+// =========================================================================
+
+var LIST = /[^\s,]+/g;
+
+base2["#name"] = "base2";
+window["#name"] = "global";
+
+forEach (base2.exports.match(LIST), function(name) {
+  var property = this[name];
+  if (property instanceof Function || property instanceof Package) {
+    property["#name"] = this["#name"] + "." + name;
+    if (property instanceof Package) {
+      doc.show[name] = true;
+      property.imports = property.imports.split(",").join(", ");
+      property.exports = property.exports.split(",").join(", ");
+      forEach (property.exports.match(LIST), arguments.callee, property);
+      forEach (property, function(klass, name) {
+        if (Base.ancestorOf(klass) && !klass['#name']) {
+          klass['#name'] = property['#name'] + "." + name;
+        }
+      });
+    } else if (Module.ancestorOf(property)) {
+      forEach (property["#implements"], function(module) {
+        forEach (module, function(method, name) {
+          if (!Module[name] && typeOf(method) == "function" && property[name]) {
+            property[name]._module = module;
+          }
+        });
+      });
+    } else if (Collection.ancestorOf(property)) {
+      var Item = property.Item;
+      if (Item && !Item["#name"]) {
+        Item['#name'] = property['#name'] + ".Item";
+      }
+    }
+  }
+}, base2);
+
+base2.exports = base2.exports.split(",").join(", ");
+
+eval(this.exports);
+
+}; ////////////////////  END: CLOSURE  /////////////////////////////////////
+
+base2.doc.show = {
+  base2:      true,
+  lang:       true,
+  JavaScript: true,
+  DOM:        true,
+  IO:         true,
+  JSB:        true,
+  chrome:     true
+};
